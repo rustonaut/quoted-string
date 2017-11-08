f7d2bfe7004c11eac5885f858fffb0bc648bbe3b
meh <meh@1aim.com>
2017-11-08 17:20:48 +0100

    chore: clean up README and Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,30 @@
-//! This crate provides utilities to handle quoted strings as they appear in multiple
-//! mail and web related RFCs. While it is mainly based on rfc5322 (Internet Message Format).
-//! It also supports Utf8 based on rfc6532 (optional) and is compatible with quoted-strings
-//! as they appear in mime types (including in HTTP/1.1 context).
+//! This crate provides utilities to handle quoted strings as they appear in
+//! multiple mail and web related RFCs. While it is mainly based on RFC5322
+//! (Internet Message Format).  It also supports Utf8 based on RFC6532 (optional)
+//! and is compatible with quoted-strings as they appear in mime types (including
+//! in HTTP/1.1 context).
 //!
-//! What it currently does not support are soft-line breaks of rfc5322,
-//! neither is the obsolete part of the syntax supported (for now).
+//! What it currently does not support are soft-line breaks of RFC5322 and the
+//! obsolete parts of the syntax.
 //!
-//! The supported grammar is:
-//!
+//! Grammar
+//! -------
 //! ```no-rust
 //! quoted-string   = DQUOTE *( *WSP qcontent) *WSP DQUOTE
+//! WSP = ' ' / '\t'
 //! qcontent = qtext / quoted-pair
 //! qtext = %d33 / %d35-91 / %d93-126 ; printable us-ascii chars not including '\\' and '"'
 //! quoted-pair = ("\" (VCHAR / WSP)) ; VCHAR are printable us-ascii chars
-//! DQUPTE = '"'
-//! WSP = ' ' / '\t'
 //! ```
 //!
-//! Note that the contained `CharType` enum has a more fine grained differentiation
-//! then aboves grammar as it can differ between chars which are unquotable, which need
-//! to be represented as a quoted-pair (`'\\'` and `'"'`) which are token chars (token of rfc2045)
-//! or tspecial chars (rfc2045) or non us-ascii chars or allowed white space chars (`'\t'` and `' '`).
-//!
-//! The obsolete syntax is is currently _not supported_. Differences would be:
+//! The obsolete syntax is currently **not supported**. Differences would be:
 //!
 //! 1. it would allow CTL's in qtext
 //! 2. it would allow quoted pairs to escape CTL's, `'\0'`, `'\n'`, `'\r'`
 //!
-//! Nevertheless this part of the syntax is obsolete and should not be generated
-//! at all. Adding opt-in support for parts parsing quoted-string is in consideration.
+//! Nevertheless this part of the syntax is obsolete and should not be generated at
+//! all. Adding opt-in support for parts parsing quoted-string is in consideration.
+
 #![warn(missing_docs)]
 
 #[macro_use]
